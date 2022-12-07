ff1faeb754a5685295fa1f20b0b4e467e3cd9fae
Ryan H <git@dredx.com>
2022-12-07 03:31:31 -0800

    fixxed boilerplate file to be clearer

--- 2022/DayX/main.cpp
+++ 2022/DayX/main.cpp
@@ -1,22 +1,23 @@
 #include "helpers/getInput.hpp"
 #include <filesystem>
+
+#define EXAMPLE_FILE 0
+#define PUZZLE_FILE 1
+
 void part1(std::vector<std::string> lines);
-void part2_no_sort(std::vector<std::string> lines);
-void findTop3(int &currentCount, int largestCalories[3]);
+void part2(std::vector<std::string> lines);
 
 int main(int argc, char **argv)
 {
-    const std::filesystem::path exePath = std::filesystem::path(argv[0]).parent_path();
-    std::vector<std::string> lines = readFile(exePath, 1);
+    std::vector<std::string> lines = readFile(EXAMPLE_FILE);
     part1(lines);
-    part2_no_sort(lines);
+    part2(lines);
     return 0;
 }
 
 void part1(std::vector<std::string> lines)
 {
 }
-void part2_no_sort(std::vector<std::string> lines)
+void part2(std::vector<std::string> lines)
 {
-
 }
