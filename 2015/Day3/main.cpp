10541c3d315ac6b3407b274d30f6b04f0871c941
Ryan H <git@dredx.com>
2022-12-10 08:53:42 -0800

    fixed bug that was doubling storage string size

--- 2015/Day3/main.cpp
+++ 2015/Day3/main.cpp
@@ -5,11 +5,11 @@
 #define EXAMPLE_FILE 0
 #define PUZZLE_FILE 1
 
-void part1(std::vector<std::string> lines);
-void part2(std::vector<std::string> lines);
+void part1(const std::vector<std::string> &lines);
+void part2(const std::vector<std::string> &lines);
 void printTotal(int total);
-void divideDirections(std::string &directions, std::string &santa, std::string &robo);
-void getCoords(std::string &directions, std::set<std::pair<int,int>> &visited_coords);
+void divideDirections(const std::string &directions, std::string &santa, std::string &robo);
+void getCoords(const std::string &directions, std::set<std::pair<int, int>> &visited_coords);
 
 int main()
 {
@@ -19,52 +19,55 @@ int main()
     return 0;
 }
 
-void part1(std::vector<std::string> lines)
+void part1(const std::vector<std::string> &lines)
 {
     int total = 0;
 
-    //get our directions from the first line of the text file
+    // get our directions from the first line of the text file
     std::string directions = lines.front();
 
-    //get a set representing the coords - sets only contain unique items
-    std::set<std::pair<int,int>> visited_coords;
+    // get a set representing the coords - sets only contain unique items
+    std::set<std::pair<int, int>> visited_coords;
     getCoords(directions, visited_coords);
 
-    //the total is the size of the coordinate set
+    // the total is the size of the coordinate set
     total = visited_coords.size();
     printTotal(total);
 }
 
-
-
-void part2(std::vector<std::string> lines)
+void part2(const std::vector<std::string> &lines)
 {
     int total = 0;
 
-    //get our directions from the first line of the text file
+    // get our directions from the first line of the text file
     std::string directions = lines.front();
 
-    //create our storage strings since we know the size initialize them
-    int sub_directions_length = directions.length()/2+1;
-    std::string santa_directions (sub_directions_length, ' ');
-    std::string robo_santa_directions (sub_directions_length, ' ');
+    // create our storage strings 
+    std::string santa_directions;
+    std::string robo_santa_directions;
 
+    //divide them for the two santas
     divideDirections(directions, santa_directions, robo_santa_directions);
 
-    //get a set representing the coords - sets only contain unique items
-    std::set<std::pair<int,int>> santa_visited_coords;
-    std::set<std::pair<int,int>> robo_santa_visited_coords;
+    // get a set representing the coords - sets only contain unique items
+    std::set<std::pair<int, int>> santa_visited_coords;
+    std::set<std::pair<int, int>> robo_santa_visited_coords;
 
+    //get the coordinates each santa visits
     getCoords(santa_directions, santa_visited_coords);
     getCoords(robo_santa_directions, robo_santa_visited_coords);
 
-    //merge our two sets into one unique set of coords.
-    std::set<std::pair<int,int>> visited_coords;
-    std::merge(santa_visited_coords.begin(), santa_visited_coords.end(),
-               robo_santa_visited_coords.begin(), robo_santa_visited_coords.end(),
-               std::inserter(visited_coords, visited_coords.begin()));
+    // merge our two sets into one unique set of coords.
+    std::set<std::pair<int, int>> visited_coords;
+    visited_coords.insert(santa_visited_coords.begin(),santa_visited_coords.end());
+    visited_coords.insert(robo_santa_visited_coords.begin(),robo_santa_visited_coords.end());
 
-    //the total is the size of the coordinate set
+    // merge is not neccesary, set can do it itself as above
+    // std::merge(santa_visited_coords.begin(), santa_visited_coords.end(),
+    //            robo_santa_visited_coords.begin(), robo_santa_visited_coords.end(),
+    //            std::inserter(visited_coords, visited_coords.begin()));
+
+    // the total is the size of the coordinate set
     total = visited_coords.size();
 
     printTotal(total);
@@ -72,24 +75,26 @@ void part2(std::vector<std::string> lines)
 
 // This will take a input where every other character in the drections goes to robo_santa
 // and split it into directions for santa and directions for robo_santa
-void divideDirections(std::string &directions, std::string &santa, std::string &robo)
+void divideDirections(const std::string &directions, std::string &santa, std::string &robo)
 {
     for (int i = 0; i < directions.length(); i++)
     {
-        if (i % 2 == 0) santa += directions[i];
-        else robo += directions[i];
+        if (i % 2 == 0)
+            santa += directions[i];
+        else
+            robo += directions[i];
     }
 }
 
-void getCoords(std::string &directions, std::set<std::pair<int,int>> &visited_coords)
+//find all the coordinates santa visits
+void getCoords(const std::string &directions, std::set<std::pair<int, int>> &visited_coords)
 {
-    std::pair<int,int> coord = {0,0};
+    std::pair<int, int> coord = {0, 0};
 
-    //add out starting position
+    // add out starting position
     visited_coords.insert(coord);
 
-    //loop through every character of line 1 in lines ( there is only 1 line)
-    for (char &d : directions)
+    for (char d : directions)
     {
         if (d == '^')
         {
@@ -114,6 +119,6 @@ void getCoords(std::string &directions, std::set<std::pair<int,int>> &visited_co
 void printTotal(int total)
 {
     static int calls = 1;
-    printf("Part%i Total: %i\n", calls,total);
+    printf("Part%i Total: %i\n", calls, total);
     calls++;
 }
