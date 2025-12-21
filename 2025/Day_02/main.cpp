fac8a7b127799169c34587c78d41596ffb7bb524
Dreded <dreded@dredx.com>
2025-12-21 15:03:24 -0800

    Finished part 1 of Day 2

--- 2025/Day_02/main.cpp
+++ 2025/Day_02/main.cpp
@@ -1,18 +1,67 @@
 #include "../../helpers/getInput.hpp"
+#include <sstream>
 
 enum FileType { EXAMPLE_FILE, PUZZLE_FILE, CUSTOM_EXAMPLE_FILE };
 
-void printTotal(int total)
+void printTotal(long long total)
 {
     static int calls = 1;
-    printf("Part%i Total: %i\n", calls,total);
+    printf("Part%i Total: %lld\n", calls,total);
     calls++;
 }
+long long rangeCheck(const std::string& range)
+{
+  long long total = 0;
+  std:: stringstream ss(range);
+  long long start, end;
+  char dash;
+  ss >> start >> dash >> end;
+  // printf("Start: %d\t End: %d\n", start, end);
+
+  for (long long i = start; i <= end; i++)
+  {
+    std::string s = std::to_string(i);
+    int len  = s.size();
+
+    // if the string length is odd its valid so skip this loop
+    if (len % 2 != 0) continue;
+    int half = len/2;
+    if (s.substr(0,half) == s.substr(half,half))
+    {
+      // its repeating/invalid
+      total += i;
+    }
+  }
+  return total;
+}
+
+std::vector<std::string> splitLine(const std::string& line)
+{
+  std::vector<std::string> ranges;
+  std::stringstream ss(line);
+  std::string token;
+
+  while (std::getline(ss, token, ',')) //split by comma
+  {
+    ranges.push_back(token);
+  }
+  
+  return ranges;
+}
+
 void part1(const InputData& input)
 {
-    int total = 0;
-    printTotal(total);
+  long long total = 0;
+  auto ranges = splitLine(input.lines[0]);
+  printf("Total Ranges Found: %ld\n", ranges.size());
+  for (const std::string& r : ranges)
+  {
+    total += rangeCheck(r);
+  }
+
+  printTotal(total);
 }
+
 void part2(const InputData& input)
 {
     int total = 0;
